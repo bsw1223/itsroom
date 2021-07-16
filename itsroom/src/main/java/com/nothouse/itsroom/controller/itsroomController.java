605cbae1dd209b858e90a42bd022eaf3e82fcce3
bsw1223 <bsw1223@daum.net>
2021-07-17 00:51:08 +0900

    gradle hibernate dependency추가
    entity 추가

--- itsroom/src/main/java/com/nothouse/itsroom/controller/itsroomController.java
+++ itsroom/src/main/java/com/nothouse/itsroom/controller/itsroomController.java
@@ -1,14 +1,15 @@
 package com.nothouse.itsroom.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class itsroomController {
 	
 	@GetMapping("itsroom")
-	public String itsroomController() {
-
+	public String itsroomController(Model model) {
+        model.addAttribute("data","itsroom");
 		return "itsroom";
 	}
 }
